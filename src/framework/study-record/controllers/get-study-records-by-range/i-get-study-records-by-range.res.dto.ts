ea49a72c08c8140d071c7fc52986959c407585d4
KeonU Kim <anythingthannothing@gmail.com>
2025-01-23 18:57:07 +0900

    fix: update get study record by range response body

--- src/framework/study-record/controllers/get-study-records-by-range/i-get-study-records-by-range.res.dto.ts
+++ src/framework/study-record/controllers/get-study-records-by-range/i-get-study-records-by-range.res.dto.ts
@@ -7,6 +7,8 @@ export interface IGetStudyRecordByDateResDto {
 
 interface Study {
   title: string;
+  color: string;
+  order: number;
   duration: number;
   totalBreak: number;
   startAt: number;
